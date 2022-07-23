9ec4ec4a24dfef8922c3e1683fa38595c97923f3
BlackBeard009 <princesagor159@gmail.com>
2022-07-23 17:05:45 +0600

    Create 704A.cpp

---
+++ Codeforces/Ad-hok/704A.cpp
@@ -0,0 +1,96 @@
+/*#pragma GCC optimize("Ofast,unroll-loops")
+#pragma GCC target("avx,avx2,fma")
+*/
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define pf push_front
+#define ppb pop_back
+#define ppf pop_front
+#define powr(x,n,p) for(int axy=0 ; axy<p ; axy++){x=x*n;}
+#define YES cout<<"YES\n"
+#define Yes cout<<"Yes\n"
+#define yes cout<<"yes\n"
+#define NO cout<<"NO\n"
+#define No cout<<"No\n"
+#define no cout<<"no\n"
+#define nl cout<<"\n"
+#define fr0(n,q) for(q=0 ; q<n ; q++)
+#define fr1(n,q) for(q=1 ; q<=n ; q++)
+#define mp make_pair
+#define scl(x) scanf("%lld",&x)
+#define sci(x) scanf("%d",&x)
+#define lst(x) x[x.size()-1]
+#define llst(x) x[x.size()-2]
+#define md 1000000007
+#define check(x,y) (x&(1LL<<y))
+#define set(x,y) x=x|(1LL<<y)
+#define pi acos(-1)
+#define inf (1<<30)
+#define N  300005
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define pi acos(-1)
+using namespace std;
+/*#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+//*s.find_by_order(2) -> element at index 2
+//s.order_of_key(3) -> lower bound of 3
+
+/*mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+int gen_random(int l, int r) {
+    return uniform_int_distribution<int>(l, r)(rng);
+}*/
+
+vector < pair<int,int> > v[N];
+int n,m;
+set < pair<int,int> > st; 
+
+int main()
+{
+    /*freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);*/
+    
+
+    int i,j;
+
+    cin>>n>>m;
+
+    int cnt=1,ans=0;
+
+    for(i=1 ; i<=m ; i++){
+        int a,b;
+        cin>>a>>b;
+        if(a==1){
+            st.insert({cnt,b});
+            v[b].pb({cnt,b});
+            cnt++;
+            ans++;
+        }
+        else if(a==2){
+            for(auto it : v[b]){
+                if(st.find(it)!=st.end()){
+                    st.erase(it);
+                    ans--;
+                }
+            }
+            v[b].clear();
+        }
+        else{
+            while(st.size() && (*st.begin()).first<=b){
+                st.erase(st.begin());
+                ans--;
+            }
+        }
+
+        cout<<ans;
+        nl;
+
+    }
+    
+
+    return 0;
+    
+
+}
